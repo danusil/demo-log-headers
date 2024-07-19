21143195a68f113216fd946ba1b0ad0749fe9a92
DNU@TRUE <>
2024-07-19 10:29:45 +0700

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 
 export default function Home() {
-  const [cookies, setCookies] = useState(["theme"]);
-
-  useEffect(() => {
-    setCookies(document.cookie.split(";"));
-  }, [])
+  const [cookies, setCookie] = useCookies();
 
   return (
     <div>
       <h1>Home</h1>
-      <p>Cookie: {cookies}</p>
+      <p>Cookie: {cookies["trueapp-device-id"]}</p>
     </div>
   );
 }
